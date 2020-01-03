dc8757dc241c5beb7b06a4f84ed9ed79b6f7fff0
Aivo Antsman <aivo.antsman@gmail.com>
2020-01-03 16:13:24 +0200

    Update get-versions.sh, use numeric ver

--- get-versions.sh
+++ get-versions.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-test -n "$1" && (
-    echo MONGO_VERSION=\"$(docker exec -t $1 mongod --version | grep -e 'db version' | awk '{ print $3 }' | tr -d 'v\r')\"
-    echo DEBIAN_VERSION=\"$(docker exec -t $1 grep VERSION= /etc/os-release|awk '{ print $2 }' | tr -d '()"\r')\"
-) | tee env.properties || (
-    echo Give container name as parameter!
-    exit 1
-)
+VERSIONS=version.properties
 
+test -n "$1" && (
+  echo MONGO_VERSION=\"$(docker exec -t $1 mongod --version | grep -e 'db version' | awk '{ print $3 }' | tr -d 'v\r')\" | tee -a $VERSIONS
+
+  OS_ID=$(docker exec -t $1 cat /etc/os-release | grep -G ^ID= | awk -F= '{ print $2 }' | tr -d '\r')
+  OS_VERSION_ID=$(docker exec -t $1 cat /etc/os-release | grep -G VERSION_ID= | awk -F= '{ print $2 }' | tr -d '"\r')
+  echo OS_VERSION=\"${OS_ID}-${OS_VERSION_ID}\" | tee -a $VERSIONS
+) | tee env.properties || (
+  echo Give container name as parameter!
+  exit 1
+)
